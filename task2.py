4dcd815139cab98abdb9dfde52cbfe987eaf8690
MarcTucay123 <marctucay111@gmail.com>
2025-02-21 09:13:41 +0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -7,12 +7,12 @@ class Student:
     def introduce(self):
         print(f"Hello, my name is {self.name}, I am {self.age} years old, and I am studying {self.course}.")
 
-# Create three student objects
+
 student1 = Student("Halaena", 20, "Computer Science")
 student2 = Student("Ekko", 22, "Computer Engineering")
 student3 = Student("Camille", 21, "Information Technology")
 
-# Call their introduce() method
+
 student1.introduce()
 student2.introduce()
 student3.introduce()
